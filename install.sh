2062ba7adc42f0f11eb4cfd5f82fe69feadaf2cb
c404t <aaeicsa@gmail.com>
2024-03-28 22:28:44 +0330

    install.sh update

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-#0. set location variables (i3, i3blocks/status, picom, scripts)
+# location variables (i3/i3bg, i3blocks/status, picom, scripts)
 path=$(pwd)
 i3="/home/$USER/.config/i3"
 i3bg="/home/$USER/.config/i3bg"
@@ -12,18 +12,17 @@ scripts="/home/$USER/.config/scripts"
 
 i3_file="config"
 i3bg_file="background"
-#i3blocks_file=
-#i3status_file=
-#picom_file=
+i3blocks_file="i3blocks.conf"
+i3status_file="i3status.conf"
+picom_file="picom.conf"
 
-#update=
-#disk=
-#lock=
-#memory=
-#volume=
+update="arch-update"
+disk="disk"
+lock="lock"
+memory="memory"
+volume="volume"
 
-
-#1. check if folders exists and make if they dont
+# folders set up
 if [ ! -d "$i3" ]; then
     mkdir -p "$i3"
     echo "folder '$i3' created!"
@@ -78,12 +77,11 @@ sleep 0.5
 echo "all folders are set up! moving to the next step..."
 sleep 3
 
-#check if files exists
-
+# files set up
 if [ -f "$i3/$i3_file" ]; then
     # If the file exists, rename it
-    mv "$i3/$i3_file" "$i3/${i3_file}.old"
-    echo "$i3_file file renamed to ${i3_file}.old"
+    mv "$i3/$i3_file" "$i3/${i3_file}.usr.old"
+    echo "$i3_file file renamed to ${i3_file}.usr.old"
 fi
     # If the file does not exist, create a new file
     cp "$path/i3/config"  "$i3"
@@ -91,16 +89,74 @@ fi
 
 if [ -f "$i3bg/$i3bg_file" ]; then
     # If the file exists, rename it
-    mv "$i3bg/$i3bg_file" "$i3bg/${i3bg_file}.old"
-    echo "$i3bg_file file renamed to ${i3bg_file}.old"
+    mv "$i3bg/$i3bg_file" "$i3bg/${i3bg_file}.usr.old"
+    echo "$i3bg_file file renamed to ${i3bg_file}.usr.old"
 fi
     # If the file does not exist, create a new file
     cp "$path/i3bg/background"  "$i3bg"
     echo "$path/i3bg/background copied to path: $i3bg"
 
+if [ -f "$i3blocks/$i3blocks_file" ]; then
+    mv "$i3blocks/$i3blocks_file" "$i3blocks/${i3blocks_file}.usr.old"
+    echo "$i3blocks_file file renamed to ${i3blocks_file}.usr.old"
+fi
+    cp "$path/i3blocks/i3blocks.conf" "$i3blocks"
+    echo "$path/i3blocks/i3blocks.conf copied to path: $i3blocks"
 
-#show success message if done
+if [ -f "$i3status/$i3status_file" ]; then
+    mv "$i3status/$i3status_file" "$i3status/${i3status_file}.usr.old"
+    echo "$i3status_file file renamed to ${i3status_file}.usr.old"
+fi
+    cp "$path/i3status/i3status.conf" "$i3status"
+    echo "$path/i3status/i3status.conf copied to path: $i3status"
 
-#3. ask for reloading environment
+if [ -f "$picom/$picom_file" ]; then
+    mv "$picom/$picom_file" "$picom/${picom_file}.usr.old"
+    echo "$picom_file file renamed to ${picom_file}.usr.old"
+fi
+    cp "$path/picom/picom.conf" "$picom/picom.conf"
+    echo "$path/picom/picom.conf copied to path: $picom"
+
+if [ -f "$scripts/$update" ]; then
+    mv "$scripts/$update" "$scripts/${update}.usr.old"
+    echo "$update file renamed to ${update}.usr.old"
+fi
+    cp "$path/scripts/arch-update" "$scripts"
+    echo "$path/scripts/arch-update copied to path: $scripts"
+
+if [ -f "$scripts/$disk" ]; then
+    mv "$scripts/$disk" "$scripts/${disk}.usr.old"
+    echo "$disk file renamed to ${disk}.usr.old"
+fi
+    cp "$path/scripts/disk" "$scripts"
+    echo "$path/scripts/disk copied to path: $scripts"
+
+if [ -f "$scripts/$lock" ]; then
+    mv "$scripts/$lock" "$scripts/${lock}.usr.old"
+    echo "$lock file renamed to ${lock}.usr.old"
+fi
+    cp "$path/scripts/lock" "$scripts"
+    echo "$path/scripts/lock copied to path: $scripts"
+
+if [ -f "$scripts/$memory" ]; then
+    mv "$scripts/$memory" "$scripts/${memory}.usr.old"
+    echo "$memory file renamed to ${memory}.usr.old"
+fi
+    cp "$path/scripts/memory" "$scripts"
+    echo "$path/scripts/memory copied to path: $scripts"
+
+if [ -f "$scripts/$volume" ]; then
+    mv "$scripts/$volume" "$scripts/${volume}.usr.old"
+    echo "$volume file renamed to ${volume}.usr.old"
+fi
+    cp "$path/scripts/volume" "$scripts"
+    echo "$path/scripts/volume copied to path: $scripts"
+
+#show success message
+BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+
+echo -e "${BLUE}Everything has been successfully set up!${NC}"
+echo -e "${BLUE}Reaload your i3 config now. (Alt+Shift+R)${NC}"
 
 #done
